66988fbf49ef32cae43cd4fe9a58edf355e9cd05
vitaminncpp <akshaykumarparmar51@gmail.com>
2024-06-04 01:02:28 +0530

    - initial board configuration.  - piece icons.  - UI for initial board.

--- src/app/components/chessboard/chessboard.component.ts
+++ src/app/components/chessboard/chessboard.component.ts
@@ -2,85 +2,28 @@ import { Component } from "@angular/core";
 import { AppData } from "../../data/app.data";
 import { Game } from "../../chess/game";
 import { Chessboard } from "../../chess/Chessboard";
+import GlobalConfig from "../../config/global.config";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-chessboard",
   standalone: true,
   templateUrl: "./chessboard.component.html",
   styleUrl: "./chessboard.component.scss",
-  providers: [AppData],
+  imports: [CommonModule],
+  providers: [AppData, GlobalConfig],
 })
 export class ChessboardComponent {
   protected game: Game;
   protected chessboard: Chessboard;
-  protected board: Array<Array<{ color: boolean; piece: -1 | 0 | 1 | 2 | 3 | 4 | 5 } | null>>;
+  protected board: Array<Array<{ color: boolean; piece: 0 | 1 | 2 | 3 | 4 | 5 } | null>>;
 
-  constructor(protected appData: AppData) {
+  constructor(
+    protected appData: AppData,
+    protected globalConfig: GlobalConfig
+  ) {
     this.game = new Game();
     this.chessboard = this.game.getBoard();
-    this.board = [
-      [
-        { color: false, piece: 3 },
-        { color: false, piece: 1 },
-        { color: false, piece: 2 },
-        { color: false, piece: 4 },
-        { color: false, piece: 5 },
-        { color: false, piece: 2 },
-        { color: false, piece: 1 },
-        { color: false, piece: 3 },
-      ],
-      [
-        { color: false, piece: -1 },
-        { color: false, piece: -1 },
-        { color: false, piece: -1 },
-        {
-          color: false,
-          piece: -1,
-        },
-        { color: false, piece: -1 },
-        { color: false, piece: -1 },
-        { color: false, piece: -1 },
-        {
-          color: false,
-          piece: -1,
-        },
-      ],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null],
-      [
-        { color: false, piece: -1 },
-        { color: false, piece: -1 },
-        { color: false, piece: -1 },
-        {
-          color: false,
-          piece: -1,
-        },
-        { color: false, piece: -1 },
-        { color: false, piece: -1 },
-        { color: false, piece: -1 },
-        {
-          color: false,
-          piece: -1,
-        },
-      ],
-      [
-        { color: false, piece: -1 },
-        { color: false, piece: -1 },
-        { color: false, piece: -1 },
-        {
-          color: false,
-          piece: -1,
-        },
-        { color: false, piece: -1 },
-        { color: false, piece: -1 },
-        { color: false, piece: -1 },
-        {
-          color: false,
-          piece: -1,
-        },
-      ],
-    ];
+    this.board = globalConfig.initialPosition;
   }
 }
---
+++ src/app/config/global.config.ts
@@ -0,0 +1,50 @@
+export default class GlobalConfig {
+  public tileSize = 50;
+
+  initialPosition: Array<Array<{ color: boolean; piece: 0 | 1 | 2 | 3 | 4 | 5 } | null>> = [
+    [
+      { color: true, piece: 3 },
+      { color: true, piece: 1 },
+      { color: true, piece: 2 },
+      { color: true, piece: 4 },
+      { color: true, piece: 5 },
+      { color: true, piece: 2 },
+      { color: true, piece: 1 },
+      { color: true, piece: 3 },
+    ],
+    [
+      { color: true, piece: 0 },
+      { color: true, piece: 0 },
+      { color: true, piece: 0 },
+      { color: true, piece: 0 },
+      { color: true, piece: 0 },
+      { color: true, piece: 0 },
+      { color: true, piece: 0 },
+      { color: true, piece: 0 },
+    ],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [
+      { color: false, piece: 0 },
+      { color: false, piece: 0 },
+      { color: false, piece: 0 },
+      { color: false, piece: 0 },
+      { color: false, piece: 0 },
+      { color: false, piece: 0 },
+      { color: false, piece: 0 },
+      { color: false, piece: 0 },
+    ],
+    [
+      { color: false, piece: 3 },
+      { color: false, piece: 1 },
+      { color: false, piece: 2 },
+      { color: false, piece: 5 },
+      { color: false, piece: 4 },
+      { color: false, piece: 2 },
+      { color: false, piece: 1 },
+      { color: false, piece: 3 },
+    ],
+  ];
+}
