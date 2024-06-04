409e2eae5a2c3f73919215f88daac14dc340647f
vitaminncpp <akshaykumarparmar51@gmail.com>
2024-06-05 00:36:04 +0530

    - Added JQuery and Jquery-ui to Project for drag and drop

--- src/app/components/chessboard/chessboard.component.ts
+++ src/app/components/chessboard/chessboard.component.ts
@@ -1,9 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, SimpleChanges } from "@angular/core";
 import { AppData } from "../../data/app.data";
 import { Game } from "../../chess/game";
 import { Chessboard } from "../../chess/Chessboard";
 import GlobalConfig from "../../config/global.config";
 import { CommonModule } from "@angular/common";
+import $ from "jquery";
+import "jqueryui";
 
 @Component({
   selector: "app-chessboard",
@@ -17,6 +19,7 @@ export class ChessboardComponent {
   protected game: Game;
   protected chessboard: Chessboard;
   protected board: Array<Array<{ color: boolean; piece: 0 | 1 | 2 | 3 | 4 | 5 } | null>>;
+  protected orientation: boolean = true;
 
   constructor(
     protected appData: AppData,
@@ -25,5 +28,21 @@ export class ChessboardComponent {
     this.game = new Game();
     this.chessboard = this.game.getBoard();
     this.board = globalConfig.initialPosition;
+  }
+  ngOnInit() {}
+  ngAfterContentChecked(): void {
+    console.log("changes");
+    $(".piece").draggable({
+      // containment: "table.grid#chessgrid", // constraints the drag to the chessboard only
+      // snap: "table.grid#chessgrid td",
+      // revert: true,
+    });
+    // $("table.grid td").droppable({
+    //   drop: (e: any, ui: any) => {
+    //     e.target.innerHTML = "";
+    //     e.target.appendChild(ui.draggable[0]);
+    //     ui.draggable.css({ top: "0px", left: "0px" });
+    //   },
+    // });
   }
 }
