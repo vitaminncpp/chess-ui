3af0817a953f9867b7356a98ed222b09ab5ad504
vitaminncpp <akshaykumarparmar51@gmail.com>
2024-06-06 18:08:27 +0530

    - Added static constants for all piece type and values in global.config.

--- src/app/config/global.config.ts
+++ src/app/config/global.config.ts
@@ -1,50 +1,68 @@
 export default class GlobalConfig {
+  public PAWN_VALUE = 1 as const;
+  public PAWN_TYPE = 0 as const;
+
+  public KNIGHT_VALUE = 3 as const;
+  public KNIGHT_TYPE = 1 as const;
+
+  public BISHOP_VALUE = 3 as const;
+  public BISHOP_TYPE = 2 as const;
+
+  public ROOK_VALUE = 5 as const;
+  public ROOK_TYPE = 3 as const;
+
+  public QUEEN_VALUE = 9 as const;
+  public QUEEN_TYPE = 4 as const;
+
+  public KING_VALUE = 10000 as const;
+  public KING_TYPE = 5 as const;
+
   public tileSize = 80;
 
   initialPosition: Array<Array<{ color: boolean; piece: 0 | 1 | 2 | 3 | 4 | 5 } | null>> = [
     [
-      { color: true, piece: 3 },
-      { color: true, piece: 1 },
-      { color: true, piece: 2 },
-      { color: true, piece: 4 },
-      { color: true, piece: 5 },
-      { color: true, piece: 2 },
-      { color: true, piece: 1 },
-      { color: true, piece: 3 },
+      { color: true, piece: this.ROOK_TYPE },
+      { color: true, piece: this.KNIGHT_TYPE },
+      { color: true, piece: this.BISHOP_TYPE },
+      { color: true, piece: this.QUEEN_TYPE },
+      { color: true, piece: this.KING_TYPE },
+      { color: true, piece: this.BISHOP_TYPE },
+      { color: true, piece: this.KNIGHT_TYPE },
+      { color: true, piece: this.ROOK_TYPE },
     ],
     [
-      { color: true, piece: 0 },
-      { color: true, piece: 0 },
-      { color: true, piece: 0 },
-      { color: true, piece: 0 },
-      { color: true, piece: 0 },
-      { color: true, piece: 0 },
-      { color: true, piece: 0 },
-      { color: true, piece: 0 },
+      { color: true, piece: this.PAWN_TYPE },
+      { color: true, piece: this.PAWN_TYPE },
+      { color: true, piece: this.PAWN_TYPE },
+      { color: true, piece: this.PAWN_TYPE },
+      { color: true, piece: this.PAWN_TYPE },
+      { color: true, piece: this.PAWN_TYPE },
+      { color: true, piece: this.PAWN_TYPE },
+      { color: true, piece: this.PAWN_TYPE },
     ],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null, null],
     [
-      { color: false, piece: 0 },
-      { color: false, piece: 0 },
-      { color: false, piece: 0 },
-      { color: false, piece: 0 },
-      { color: false, piece: 0 },
-      { color: false, piece: 0 },
-      { color: false, piece: 0 },
-      { color: false, piece: 0 },
+      { color: false, piece: this.PAWN_TYPE },
+      { color: false, piece: this.PAWN_TYPE },
+      { color: false, piece: this.PAWN_TYPE },
+      { color: false, piece: this.PAWN_TYPE },
+      { color: false, piece: this.PAWN_TYPE },
+      { color: false, piece: this.PAWN_TYPE },
+      { color: false, piece: this.PAWN_TYPE },
+      { color: false, piece: this.PAWN_TYPE },
     ],
     [
-      { color: false, piece: 3 },
-      { color: false, piece: 1 },
-      { color: false, piece: 2 },
-      { color: false, piece: 4 },
-      { color: false, piece: 5 },
-      { color: false, piece: 2 },
-      { color: false, piece: 1 },
-      { color: false, piece: 3 },
+      { color: false, piece: this.ROOK_TYPE },
+      { color: false, piece: this.KNIGHT_TYPE },
+      { color: false, piece: this.BISHOP_TYPE },
+      { color: false, piece: this.QUEEN_TYPE },
+      { color: false, piece: this.KING_TYPE },
+      { color: false, piece: this.BISHOP_TYPE },
+      { color: false, piece: this.KNIGHT_TYPE },
+      { color: false, piece: this.ROOK_TYPE },
     ],
   ];
 }
