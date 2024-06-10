2dec45bc5ac7538f01f78ef9c3d76ccb529d93bf
vitaminncpp <akshaykumarparmar51@gmail.com>
2024-06-10 23:38:04 +0530

    - Updated GlobalConfig and Appdata formats.

--- src/app/config/global.config.ts
+++ src/app/config/global.config.ts
@@ -1,7 +1,12 @@
-export default class GlobalConfig {
-  public tileSize = 80;
+export interface GlobalConfig {
+  tileSize: number;
+  initialPosition: Array<Array<{ color: boolean; piece: 0 | 1 | 2 | 3 | 4 | 5 } | null>>;
+}
 
-  initialPosition: Array<Array<{ color: boolean; piece: 0 | 1 | 2 | 3 | 4 | 5 } | null>> = [
+export const globalConfig: GlobalConfig = {
+  tileSize: 80,
+
+  initialPosition: [
     [
       { color: true, piece: 3 },
       { color: true, piece: 1 },
@@ -46,5 +51,5 @@ export default class GlobalConfig {
       { color: false, piece: 1 },
       { color: false, piece: 3 },
     ],
-  ];
-}
+  ],
+};
